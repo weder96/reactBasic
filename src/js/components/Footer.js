cbf4bcb60dd755600007237f902e5b29eca49945
weder.sousa <weder.sousa@squadra.com.br>
2019-04-17 17:30:25 -0300

    initial commit

---
+++ src/js/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Footer extends React.Component {
+    render() {
+        return (
+            /* jshint ignore:start */
+            <footer>footer</footer>
+            /* jshint ignore:end */
+        );
+    }
+}
